d2822cf3175e1b294c63ff1883551af4e12d1155
adnan118 <73432559+adnan118@users.noreply.github.com>
2025-03-10 03:33:27 -0700

    Update server.js

--- server.js
+++ server.js
@@ -12,9 +12,34 @@ const PORT = process.env.PORT || 3000;
 // app.use(express.urlencoded({ extended: false }));  // هذا مطلوب لـ x-www-form-urlencoded
 app.use(express.json());
  
+ const mysql = require("mysql2/promise");
+
+// إعداد اتصال قاعدة البيانات
+/*
+const dbConfig = {
+  host: "mysql11.serv00.com",
+  user: "m1241_yabro",
+  password: "Yabro.2025",
+  database: "m1241_yabro" 
+};
+*/
+const dbConfig = {  
+  host: process.env.DB_HOST,  
+  user: process.env.DB_USER,  
+  password: process.env.DB_PASSWORD,  
+  database: process.env.DB_DATABASE  
+};  
+// دالة للحصول على اتصال بقاعدة البيانات
+async function getConnection() {
+ res.json({ message: "Hello, conn!" });  
+  return await mysql.createConnection(dbConfig);
  
+}
+
+
 app.get("/", (req, res) => {  
-res.json({ message: "Hello, world!" });  
+res.json({ message: "Hello, world!" });   getConnection();
+ 
  });  
 app.use("/api118259y", LoginUserRout);
 app.use("/api118259y", UserRoutes);
