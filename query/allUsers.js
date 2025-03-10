0e7726294ad6d64fcda8892e6c6685d3d775a886
adnan118 <73432559+adnan118@users.noreply.github.com>
2025-03-10 02:27:38 -0700

    Update allUsers.js

--- query/allUsers.js
+++ query/allUsers.js
@@ -1,30 +1,21 @@
 const { getAllData } = require("../controllers/functions");
- 
-const { getConnection } = require("./db");
 
 // دالة لجلب بيانات منتج بناءً على معرفه
 async function AllUsers(req, res) {
   try {
-    // الحصول على اتصال من تجمع الاتصالات
-    const connection = await getConnection(); // الحصول على الاتصال
-
-    // الاستعلام عن جميع المستخدمين
-    const [results] = await connection.execute("SELECT * FROM users");
-
-    // تحرير الاتصال بعد الانتهاء
-    connection.release();
-
-    res.json({ status: "success", data: results }); // هيكل موحد
-    console.log(results);
+    const result = await getAllData("users");
+    res.json(result);
+    console.log(result);
   } catch (error) {
-    console.error("Database query error: ", error);
-    res.status(500).json({
-      status: "failure",
-      message: "There is a problem retrieving data",
-    });
+    console.error("Error fetching data: ", error);
+    res
+      .status(500)
+      .json({
+        status: "failure",
+        message: "There is a problem retrieving data",
+      });
   }
-}  
-
+}
 
 // تصدير الدوال
 module.exports = { AllUsers };
