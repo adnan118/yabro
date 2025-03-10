32601ef519d810706ffc4c1cc2766911eb6bf792
adnan118 <73432559+adnan118@users.noreply.github.com>
2025-03-10 03:57:48 -0700

    Update server.js

--- server.js
+++ server.js
@@ -14,13 +14,18 @@ app.use(express.json());
 const mysql = require("mysql2/promise");  
 
 // إعداد اتصال قاعدة البيانات  
-const dbConfig = {  
+/*const dbConfig = {  
 host: "mysql11.serv00.com",
   user: "m1241_smc",
   password: "l#^yVeC2PQgQ!Wd@rPxd",
   database: "m1241_yabrov",
+};  */
+  const dbConfig = {  
+  host: process.env.DB_HOST,  
+  user: process.env.DB_USER,  
+  password: process.env.DB_PASSWORD,  
+  database: process.env.DB_DATABASE  
 };  
-  
 // دالة للحصول على اتصال بقاعدة البيانات  
 async function getConnection() {  
   try {  
