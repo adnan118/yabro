0b66d2f7c33e136b97763558f13644401e861a3b
adnan118 <73432559+adnan118@users.noreply.github.com>
2025-03-10 01:37:16 -0700

    server.js

--- server.js
+++ server.js
@@ -1,15 +1,23 @@
-var http = require("http");  
-let port = process.env.PORT || 3000;
-let hostname = "0.0.0.0";  
-let server = http.createServer(function (request, response) {  
+// server.js
 
-  response.write('hello');  
-  response.setHeader('Content-Type', 'text/plain');  
-  response.end();  
+const express              = require("express");
+const userRoutes           = require("./routes/allUsers");
+const LoginUserRout        = require("./routes/authRoutes/LoginUserRout");
+const RegisterUserRout     = require("./routes/authRoutes/RegisterUserRout");
+const VerifyUserRout       = require("./routes/authRoutes/VfcRout");
+const ResendVerifyUserRout = require("./routes/authRoutes/ResendVfcUserRout");
 
-}) ;  
+const app = express();
+const PORT = process.env.PORT || 3000;
+// app.use(express.urlencoded({ extended: false }));  // هذا مطلوب لـ x-www-form-urlencoded
+app.use(express.json());
 
-server.listen(port, hostname, function () {  
-  console.log("http://" + hostname + ":" + port);  
-});  
+app.use("/api118259y", LoginUserRout);
+app.use("/api118259y", userRoutes);
+app.use("/api118259y", RegisterUserRout);
+app.use("/api118259y", VerifyUserRout);
+app.use("/api118259y", ResendVerifyUserRout);
 
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
