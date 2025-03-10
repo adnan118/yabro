6daed3b4e68afe416e1307dde89c070ba216e9c4
adnan118 <73432559+adnan118@users.noreply.github.com>
2025-03-10 03:38:32 -0700

    Update server.js

--- server.js
+++ server.js
@@ -15,10 +15,10 @@ const mysql = require("mysql2/promise");
 
 // إعداد اتصال قاعدة البيانات  
 const dbConfig = {  
-  host: process.env.DB_HOST,  
-  user: process.env.DB_USER,  
-  password: process.env.DB_PASSWORD,  
-  database: process.env.DB_DATABASE  
+host: "mysql11.serv00.com",
+  user: "m1241_yabro",
+  password: "Yabro.2025",
+  database: "m1241_yabro",
 };  
 
 // دالة للحصول على اتصال بقاعدة البيانات  
