56dd988375b0d2d7f3498be8bed096dcd2fc0861
adnan118 <73432559+adnan118@users.noreply.github.com>
2025-03-10 02:16:40 -0700

    Update allUsers.js

--- query/allUsers.js
+++ query/allUsers.js
@@ -3,7 +3,15 @@ const { getAllData } = require("../controllers/functions");
 // دالة لجلب بيانات منتج بناءً على معرفه
 async function AllUsers(req, res) {
   try {
+     
+    res
+      .status(500)
+      .json({
+        status: "1",
+        message: "1 q",
+      });
     const result = await getAllData("users");
+    
     res.json(result);
     console.log(result);
   } catch (error) {
