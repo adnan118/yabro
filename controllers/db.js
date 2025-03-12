8bb53d2f61ec8e204872bf062ae4c9ec5f0a0738
adnan ahmad al barakat <73432559+adnan118@users.noreply.github.com>
2025-03-12 06:20:18 -0700

    Update db.js

--- controllers/db.js
+++ controllers/db.js
@@ -1,4 +1,4 @@
-/*// controllers/database.js
+// controllers/database.js
 const mysql = require("mysql2/promise");
 
  
@@ -15,7 +15,7 @@ const dbConfig = {
   user: process.env.DB_USER,  
   password: process.env.DB_PASSWORD,  
   database: process.env.DB_DATABASE  
-};  
+};  */
 // دالة للحصول على اتصال بقاعدة البيانات
 async function getConnection() {
   return await mysql.createConnection(dbConfig);
@@ -26,20 +26,4 @@ async function getConnection() {
 // تصدير الدالة
 module.exports = { getConnection };
 
-
-*/
-
-// controllers/database.js  
-require("dotenv").config();  
-const { neon } = require("@neondatabase/serverless");  
-
-const sql = neon(DATABASE_URL='postgresql://neondb_owner:npg_TBcjMd8h9lqp@ep-curly-queen-a26ytq8m-pooler.eu-central-1.aws.neon.tech/neondb?sslmode=require'); // استخدم رابط قاعدة البيانات من .env  
-
-// دالة لتنفيذ استعلام  
-async function executeQuery(query) {  
-  const result = await sql`${query}`;  
-  return result;  
-}  
-
-// تصدير الدالة  
-module.exports = { executeQuery };  
+ 
