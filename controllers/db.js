580d8b464bcb2c87c2a36d76fc457b63c8559715
adnan ahmad al barakat <73432559+adnan118@users.noreply.github.com>
2025-03-12 06:28:18 -0700

    Update db.js

--- controllers/db.js
+++ controllers/db.js
@@ -7,7 +7,7 @@ const dbConfig = {
   host: "mysql11.serv00.com",
   user: "m1241_smc",
   password: "l#^yVeC2PQgQ!Wd@rPxd",
-  database: "m1241_smc" 
+  database: "p1241_smc" 
 };
 /*
 const dbConfig = {  
