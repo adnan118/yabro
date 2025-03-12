8584e2c2d835f902bc57ebac1400eecbebefe366
adnan ahmad al barakat <73432559+adnan118@users.noreply.github.com>
2025-03-12 06:40:31 -0700

    Update db.js

--- controllers/db.js
+++ controllers/db.js
@@ -4,10 +4,10 @@ const mysql = require("mysql2/promise");
  
 
 const dbConfig = {
-  host: "pgsql11.serv00.com",
+  host: "mysql11.serv00.com",
   user: "m1241_smc",
   password: "l#^yVeC2PQgQ!Wd@rPxd",
-  database: "p1241_smc" 
+  database: "m1241_yabrov" 
 };
 /*
 const dbConfig = {  
