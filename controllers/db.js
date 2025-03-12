f62ff12ca09adbf2c87b62a9567a39d78b527d06
adnan ahmad al barakat <73432559+adnan118@users.noreply.github.com>
2025-03-12 07:36:28 -0700

    Update db.js

--- controllers/db.js
+++ controllers/db.js
@@ -2,20 +2,20 @@
 const mysql = require("mysql2/promise");
 
  
-
+/*
 const dbConfig = {
   host: "mysql11.serv00.com",
   user: "m1241_smc",
   password: "l#^yVeC2PQgQ!Wd@rPxd",
   database: "m1241_yabrov" 
 };
-/*
+*/
 const dbConfig = {  
-  host: process.env.DB_HOST,  
-  user: process.env.DB_USER,  
-  password: process.env.DB_PASSWORD,  
-  database: process.env.DB_DATABASE  
-};  */
+  host: "mysql.railway.internal",  
+  user: "rootana",  
+  password: "YrRDAzFHoLEbnSHGffiEAkrrToUKBCnD",  
+  database: "railway"
+};  
 // دالة للحصول على اتصال بقاعدة البيانات
 async function getConnection() {
   return await mysql.createConnection(dbConfig);
