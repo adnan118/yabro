e71329a29b4301a6f2bca52a551e0242a9f35662
adnan ahmad al barakat <73432559+adnan118@users.noreply.github.com>
2025-03-13 06:40:17 -0700

    Update server.js

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const LoginUserRout = require("./routes/authRoutes/LoginUserRout");
 const RegisterUserRout = require("./routes/authRoutes/RegisterUserRout");  
 const VerifyUserRout = require("./routes/authRoutes/VfcRout");  
 const ResendVerifyUserRout = require("./routes/authRoutes/ResendVfcUserRout");  
-
+const GetConnection = require("./controllers/db/getConnection");  
 const app = express();  
 const PORT = process.env.PORT || 3000;  
 app.use(express.json());  
@@ -15,7 +15,7 @@ app.use(express.json());
 
 app.get("/", async (req, res) => {  
   try {  
-    await getConnection(); // تحقق من الاتصال  
+    await GetConnection(); // تحقق من الاتصال  
     res.json({ message: "Connected to the database successfully!" });  
   } catch (error) {  
     res.status(500).json({ message: "Database connection failed." });  
