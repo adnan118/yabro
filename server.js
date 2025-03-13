e8886c61d86275ad024fe4138a4666d44432c891
adnan ahmad al barakat <73432559+adnan118@users.noreply.github.com>
2025-03-13 06:58:41 -0700

    Update server.js

--- server.js
+++ server.js
@@ -1,23 +1,26 @@
-// server.js  
+require('dotenv').config(); // تحميل المتغيرات من .env  
 const express = require("express");  
 const userRoutes = require("./routes/allUsers");  
 const loginUserRoute = require("./routes/authRoutes/LoginUserRout");  
 const registerUserRoute = require("./routes/authRoutes/RegisterUserRout");  
 const verifyUserRoute = require("./routes/authRoutes/VfcRout");  
 const resendVerifyUserRoute = require("./routes/authRoutes/ResendVfcUserRout");  
- 
-const mysql = require("mysql2/promise");
-// إعداد اتصال قاعدة البيانات  
+
+const mysql = require("mysql2/promise");  
+
+// إعداد اتصال قاعدة البيانات باستخدام المتغيرات البيئية  
 const dbConfig = {  
-host: "mysql.railway.internal",
-  user: "root",
-  password: "OJqazGQGUuBRhygpsVpDefAHKacQgKgg",
-  database: "railway" ,
+  host: process.env.DB_HOST,  
+  user: process.env.DB_USER,  
+  password: process.env.DB_PASSWORD,  
+  database: process.env.DB_NAME,  
 };  
-// دالة للحصول على اتصال بقاعدة البيانات
-async function getConnection1() {
-  return await mysql.createConnection(dbConfig);
-}
+
+// دالة للحصول على اتصال بقاعدة البيانات  
+async function getConnection1() {  
+  return await mysql.createConnection(dbConfig);  
+}  
+
 const app = express();  
 const PORT = process.env.PORT || 3000;  
 
