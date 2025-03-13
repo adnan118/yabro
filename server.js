34e63af0ac4e8be1fb48592dd16918bed062f4e2
adnan ahmad al barakat <73432559+adnan118@users.noreply.github.com>
2025-03-13 06:45:51 -0700

    Update server.js

--- server.js
+++ server.js
@@ -1,34 +1,33 @@
 // server.js  
-
 const express = require("express");  
-const UserRoutes = require("./routes/allUsers");  
-const LoginUserRout = require("./routes/authRoutes/LoginUserRout");  
-const RegisterUserRout = require("./routes/authRoutes/RegisterUserRout");  
-const VerifyUserRout = require("./routes/authRoutes/VfcRout");  
-const ResendVerifyUserRout = require("./routes/authRoutes/ResendVfcUserRout");  
-const GetConnection = require("./controllers/db/getConnection");  
+const userRoutes = require("./routes/allUsers");  
+const loginUserRoute = require("./routes/authRoutes/LoginUserRout");  
+const registerUserRoute = require("./routes/authRoutes/RegisterUserRout");  
+const verifyUserRoute = require("./routes/authRoutes/VfcRout");  
+const resendVerifyUserRoute = require("./routes/authRoutes/ResendVfcUserRout");  
+const getConnection = require("./controllers/db/getConnection");  
+
 const app = express();  
 const PORT = process.env.PORT || 3000;  
-app.use(express.json());  
 
- 
+app.use(express.json());  
 
 app.get("/", async (req, res) => {  
   try {  
-    await GetConnection(); // تحقق من الاتصال  
+    await getConnection();  
     res.json({ message: "Connected to the database successfully!" });  
   } catch (error) {  
+    console.error(error);  
     res.status(500).json({ message: "Database connection failed." });  
   }  
 });  
 
-app.use("/api118259y", LoginUserRout);  
-app.use("/api118259y", UserRoutes);  
-app.use("/api118259y", RegisterUserRout);  
-app.use("/api118259y", VerifyUserRout);  
-app.use("/api118259y", ResendVerifyUserRout);  
+app.use("/api118259y", loginUserRoute);  
+app.use("/api118259y", userRoutes);  
+app.use("/api118259y", registerUserRoute);  
+app.use("/api118259y", verifyUserRoute);  
+app.use("/api118259y", resendVerifyUserRoute);  
 
 app.listen(PORT, () => {  
   console.log(`Server is running on http://localhost:${PORT}`);  
 });  
- 
