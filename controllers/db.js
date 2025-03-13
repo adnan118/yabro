dd102851eff3f4db1315c997435d7d797ade8bca
adnan ahmad al barakat <73432559+adnan118@users.noreply.github.com>
2025-03-13 06:37:33 -0700

    Update db.js

--- controllers/db.js
+++ controllers/db.js
@@ -1,29 +1,16 @@
-// controllers/database.js
 const mysql = require("mysql2/promise");
 
- 
-/*
-const dbConfig = {
-  host: "mysql11.serv00.com",
-  user: "m1241_smc",
-  password: "l#^yVeC2PQgQ!Wd@rPxd",
-  database: "m1241_yabrov" 
-};
-*/
+// إعداد اتصال قاعدة البيانات  
 const dbConfig = {  
-  host: "mysql.railway.internal",  
-  user: "root",  
-  password: "cvDcRFBrFTDZxuYjTBKlHrddFCWfeAMw",  
-  database: "railway"
+host: "mysql.railway.internal",
+  user: "root",
+  password: "OJqazGQGUuBRhygpsVpDefAHKacQgKgg",
+  database: "railway" ,
 };  
 // دالة للحصول على اتصال بقاعدة البيانات
 async function getConnection() {
   return await mysql.createConnection(dbConfig);
 }
 
-
-
 // تصدير الدالة
 module.exports = { getConnection };
-
- 
