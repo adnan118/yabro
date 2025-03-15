6493484b8a3ac342dbd4de24dc2a5fa605d9d265
adnan ahmad al barakat <73432559+adnan118@users.noreply.github.com>
2025-03-15 15:24:58 -0700

    Add files via upload

---
+++ routes/authRoutes/FgpasswordRout.js
@@ -0,0 +1,9 @@
+// routes/routes.js
+const express = require("express");
+const { FgPassword } = require("../../query/auth/checkEmail");
+
+const router = express.Router();
+
+router.post("/fgpasswordRout", FgPassword);
+
+module.exports = router;
